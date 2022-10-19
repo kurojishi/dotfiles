687f6799d58f96783cc3746f7d2eb972fb3513a6
Francesco Berni <kurojishi@kurojishi.me>
2022-10-19 13:55:31 +0200

    dunst dependencies

--- home/run_once_801_dunst.sh
+++ home/run_once_801_dunst.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+sudo apt install libdbus-1-dev libx11-dev libxinerama-dev libxrandr-dev libxss-dev libglib2.0-dev libpango1.0-dev libgtk-3-dev libxdg-basedir-dev libnotify-dev
 cd "$HOME/src/dunst"
 
 make
