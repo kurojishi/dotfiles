b609acd56bc586f310159c426d71e3a96721a7e2
Francesco Berni <kurojishi@kurojishi.me>
2022-10-19 14:02:24 +0200

    waybar dependencies

--- home/run_once_801_waybar.sh
+++ home/run_once_801_waybar.sh
@@ -1,5 +1,31 @@
 #!/bin/bash
 
+sudo apt install \
+  clang-tidy \
+  gobject-introspection \
+  libdbusmenu-gtk3-dev \
+  libevdev-dev \
+  libfmt-dev \
+  libgirepository1.0-dev \
+  libgtk-3-dev \
+  libgtkmm-3.0-dev \
+  libinput-dev \
+  libjsoncpp-dev \
+  libmpdclient-dev \
+  libnl-3-dev \
+  libnl-genl-3-dev \
+  libpulse-dev \
+  libsigc++-2.0-dev \
+  libspdlog-dev \
+  libwayland-dev \
+  scdoc \
+  upower \
+  libxkbregistry-dev \
+  cmake \
+  meson \
+  scdoc \
+  wayland-protocols
+
 cd "$HOME/src/Waybar"
 
 meson build
