ea1a8af2efa07272235bf8578069bf15f74e569f
Francesco Berni <kurojishi@kurojishi.me>
2023-03-03 11:10:46 +0100

    updates

--- home/private_dot_config/lvim/lua/user/neovim.lua
+++ home/private_dot_config/lvim/lua/user/neovim.lua
@@ -202,7 +202,7 @@ M.config = function()
 
     -- Splitkeep
     vim.o.splitkeep = "screen"
-    vim.o.spelllang = "en_us,en_uk,it_it,jp_jp"
+    vim.o.spelllang = "en,it"
 
     -- Colorcolumn
     vim.cmd [[set colorcolumn=]]
